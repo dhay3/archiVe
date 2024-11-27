dc9d476b13a61c39f02468ed2cb433fbe4e8066c
cyberpelican <62749885+dhay3@users.noreply.github.com>
2024-11-27 10:52:59 +0800

    👷 add howto toc

--- Scripts/toc.py
+++ Scripts/toc.py
@@ -1,16 +1,14 @@
 import os
 import re
+import uuid
 import subprocess
 import urllib.parse as parse
+from typing import List, Callable
 
-toc_start_comment = "<!--toc start-->"
-toc_end_comment = "<!--toc end-->"
-placeholder = rf"{toc_start_comment}[\s\S]+{toc_end_comment}"
-base_path = "Notes/"
-readme_path = "README.md"
+README_PATH = "README.md"
 
 
-def transverse_toc(path: str, level: int, flag: bool = True) -> str:
+def transverse_dir2toc(path: str, level: int = 0, flag: bool = True) -> str:
     toc = ""
     with os.scandir(path) as objs:
         sorted_sub_dirs = sorted(
@@ -33,29 +31,56 @@ def transverse_toc(path: str, level: int, flag: bool = True) -> str:
                 md_path = parse.quote(sub_dir)
                 md_link = f"[{md_name}]({md_path})"
                 md_toc = f"## {md_link}" if flag else f'{"  " * level}- {md_link}'
-                toc += md_toc + "\n" + transverse_toc(sub_dir, level + 1, False)
+                toc += md_toc + "\n" + transverse_dir2toc(sub_dir, level + 1, False)
     return toc
 
 
-def toc2readme(path: str) -> None:
-    import uuid
+def transverse_file2toc(path: str) -> str:
+    toc = ""
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            cmd = ["git", "check-ignore", os.path.join(root, file)]
+            procs = subprocess.Popen(cmd, stdout=subprocess.DEVNULL)
+            try:
+                procs.communicate(timeout=30)
+            except subprocess.TimeoutExpired:
+                procs.kill()
+                procs.communicate()
+            if 0 != procs.returncode:
+                md_name = os.path.basename(file).split('.')[0]
+                md_path = parse.quote(os.path.join(root, file))
+                md_link = f"[{md_name}]({md_path})"
+                md_toc = f"- {md_link}"
+                toc += md_toc + "\n"
+    return toc
 
-    seed = uuid.uuid4()
-    toc = transverse_toc(base_path, 0)
+
+def to_toc_content(base_paths: List[str], toc_start_comment: str, toc_end_comment: str, func: Callable):
+    entropy = uuid.uuid4()
+    placeholder = rf"{toc_start_comment}[\s\S]+{toc_end_comment}"
+    toc = list(map(lambda _: func(_), base_paths))
     toc_content = f"""{toc_start_comment}
-<!--{seed}-->
-> [!note]
-> Created by Github Workflows
-
-{toc}
+<!--{entropy}-->
+{"".join(toc)}
 {toc_end_comment} """
-    with open(path, "r") as f:
+    with open(README_PATH, "r") as f:
         readme_content = f.read()
-    with open(path, "w") as f:
+    with open(README_PATH, "w") as f:
         n_readme_content = re.sub(placeholder, toc_content, readme_content)
         f.write(n_readme_content)
 
 
+def toc2readme() -> None:
+    print(to_toc_content(["Notes/"],
+                         "<!--notes toc start-->",
+                         "<!--notes toc end-->",
+                         transverse_dir2toc))
+
+    to_toc_content(["Howto/"],
+                   "<!--howto toc start-->",
+                   "<!--howto toc end-->",
+                   transverse_file2toc)
+
+
 if __name__ == "__main__":
-    # print(transverse_toc(base_path, 0))
-    toc2readme(readme_path)
+    toc2readme()
